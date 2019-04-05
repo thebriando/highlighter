91ef964fff87e995b440afffd4f0e6e366e1c59e
Brian Do <thebriando@gmail.com>
2019-04-05 14:52:47 -0700

    changed DOM elements to jQuery for consistency

--- js/index.js
+++ js/index.js
@@ -1,46 +1,50 @@
 // inputs
 const string =
-  "You will deliver new technology with an adorable puppy. Perfect!";
+  'You will deliver new technology with an adorable puppy. Perfect!';
 const highlights = [
   {
     startOffset: 4,
     endOffset: 20,
-    color: "#d9f593",
+    color: '#d9f593',
     priority: 0
   },
   {
     startOffset: 17,
     endOffset: 31,
-    color: "#e8e8e8",
+    color: '#e8e8e8',
     priority: 1
   },
   {
     startOffset: 37,
     endOffset: 55,
-    color: "#add8e6",
+    color: '#add8e6',
     priority: 1
   },
   {
     startOffset: 40,
     endOffset: 48,
-    color: "#98FB98",
+    color: '#98FB98',
     priority: 0
   },
   {
     startOffset: 56,
     endOffset: 64,
-    color: "#FF7F50",
+    color: '#FF7F50',
     priority: 0
   }
 ];
 
+function setText() {
+  let plainText = $('.highlight');
+  plainText.text('You will deliver new technology with an adorable puppy. Perfect!');
+  let textToHighlight = $('.result');
+  textToHighlight.text(plainText.text());
+}
+
 function highlight() {
   // sets values to string constant
-  let plainText = document.querySelector('.highlight');
-  plainText.textContent = 'You will deliver new technology with an adorable puppy. Perfect!';
-  let textToHighlight = document.querySelector(".result");
-  textToHighlight.textContent = plainText.textContent;
-  let textContent = textToHighlight.textContent;
+  let textToHighlight = $('.result');
+  let textContent = textToHighlight.text();
   // stores each substring based on each startOffset and endOffset
   let array = [];
   for (let i = 0; i < highlights.length; i++) {
@@ -61,14 +65,17 @@ function highlight() {
   }
   for (let j = 0; j < array.length; j++) {
     // creates span for highlights
-    let span = document.createElement("span");
+    let span = document.createElement('span');
     span.textContent = array[j];
     span.style.backgroundColor = highlights[j].color;
-    textToHighlight.innerHTML = textToHighlight.innerHTML.replace(
-      span.textContent,
-      span.outerHTML
-    );
+    let newText = textToHighlight.html();
+    textToHighlight.html(newText.replace(span.textContent, span.outerHTML));
   }
 }
 
-highlight();
+setText();
+
+// highlights after button is pressed
+$('button').click(() => highlight());
+// jQuery command that disables button after button is pressed
+$('button').click(() => $('button').prop('disabled', true));
